1ac56303ec81952b5de63e677020c708cecc8949
zcg <preboy@126.com>
2019-09-07 22:16:26 +0800

    signal-handler

--- main.js
+++ main.js
@@ -26,3 +26,14 @@ process.on('uncaughtException', (err) => {
 });
 
 
+// process signal
+process.on('SIGINT', beforeExit);
+process.on('SIGHUP', beforeExit);
+process.on('SIGTERM', beforeExit);
+process.on('SIGQUIT', beforeExit);
+process.on('SIGABRT', beforeExit);
+
+function beforeExit(signal) {
+    console.log(`收到信号: ${signal}`);
+    process.exit(0);
+}
